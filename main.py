ea94d92ef7deb3be91fea8812512aaf8fc904239
bibihasiba49-netizen <bibihasiba49@gmail.com>
2026-09-24 10:08:41 +0530

    Change Discord bot token in main.py
    Updated the Discord bot token in main.py.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "MTU1MjIyNDU5MjE2NzMwNTI3Ng.GNnzlA.lBVKPz6KfeEPUJgjR253FGZGuLVQcTAWM3rIKk"
+TOKEN = "MTU1MjIyNDU5MjE2NzMwNTI3Ng.GRVRfz.12MSH8UFFkAU-pAIMNNPJZxaR2WE_pwdD_Lk_Y"
 
 intents = discord.Intents.default()
 intents.message_content = True
